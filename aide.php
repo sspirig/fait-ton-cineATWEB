3702ec0a3f7f3dd980a6ac2d8e4fe3b79f20e987
Yoan-bmps <112403881+Yoan-bmps@users.noreply.github.com>
2023-10-13 09:41:09 +0200

    Correction btn / php login
    mini correction btn + php de la page login

---
+++ aide.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Script.php</title>
+</head>
+<!-- 
+    Auteur : Yoan BMPS et Santiago SPRG
+    Projet : Fais ton ciné
+    Détail : Page script
+    Date : 13.10.2023
+    Version : v1
+ -->
+<body>
+    <header>
+        <div></div> <!-- Future place ptt logo -->
+        <h1>Fais ton ciné</h1>
+        <div><a class="button" href="login.html">Login</a><a class="button" href="aide.html">Aide</a></div>
+    </header>
+    <main>
+        <section>
+            <?= "Suite..." ?>
+        </section>
+        <article>
+
+        </article>
+    </main>
+    <footer>
+        <div>
+            Auteur : Yoan BMPS & Santiago SPRG <br>
+            Projet : fais ton ciné <br>
+            Atelier: Web <br>
+        </div>
+    </footer>
+</body>
+</html>
